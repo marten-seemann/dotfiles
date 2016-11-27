176125a8e0a6fa926e2e293041f32a9fa51da7de
Marten Seemann <martenseemann@gmail.com>
2016-11-27 14:46:11 +0700

    remove unused texlive PATH

--- zsh/.zsh/01-paths.zsh
+++ zsh/.zsh/01-paths.zsh
@@ -4,6 +4,5 @@ export PATH=/usr/local/bin:$PATH
 export PATH=/usr/local/share/python:$PATH
 export PATH=/usr/local/share/npm/bin:$PATH
 export PATH=/usr/local/share/python:$PATH
-export PATH=/usr/local/texlive/2012/bin/x86_64-darwin:$PATH
 export PATH=~/.rbenv/bin:$PATH
 export PATH=~/bin:$PATH
