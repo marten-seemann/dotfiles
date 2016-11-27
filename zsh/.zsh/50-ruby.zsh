405b42a6a18aeca07a3d6476005ea743eee0c462
Marten Seemann <martenseemann@gmail.com>
2016-11-27 14:32:47 +0700

    rename deprecated Ruby env variable

--- zsh/.zsh/50-ruby.zsh
+++ zsh/.zsh/50-ruby.zsh
@@ -1,7 +1,7 @@
 if [ `which rbenv` 2> /dev/null ]; then
   eval "$(rbenv init - zsh)"
 
-  export RUBY_HEAP_MIN_SLOTS=1000000
+  export RUBY_GC_HEAP_INIT_SLOTS=1000000
   export RUBY_HEAP_SLOTS_INCREMENT=1000000
   export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
   export RUBY_GC_MALLOC_LIMIT=1000000000
