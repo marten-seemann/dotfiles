7ac6c99cb0e71fe1d8eeb104d2ff8857d5d3f0dd
Marten Seemann <martenseemann@gmail.com>
2016-11-27 15:06:13 +0700

    remove python from PATH

--- zsh/.zsh/01-paths.zsh
+++ zsh/.zsh/01-paths.zsh
@@ -1,8 +1,6 @@
 # Paths
 export PATH=/usr/local/sbin:$PATH
 export PATH=/usr/local/bin:$PATH
-export PATH=/usr/local/share/python:$PATH
 export PATH=/usr/local/share/npm/bin:$PATH
-export PATH=/usr/local/share/python:$PATH
 export PATH=~/.rbenv/bin:$PATH
 export PATH=~/bin:$PATH
