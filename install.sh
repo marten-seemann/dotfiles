4df669e7fc267df6fd029a1fec624f3bbe31ef9f
Marten Seemann <martenseemann@gmail.com>
2019-08-18 13:54:35 +0700

    add antibody installation to the install script

--- install.sh
+++ install.sh
@@ -5,7 +5,14 @@ git submodule update
 
 stow -t $HOME zsh vim git ack tmux
 
+if [[ `uname` == 'Darwin' ]]; then
+  brew install antibody
+fi
+
 if [[ `uname` == 'Linux' ]]; then
+  # install antibody
+  curl -sfL git.io/antibody | sh -s - -b /usr/local/bin
+
   echo "Setting zsh as default shell"
   chsh -s $(which zsh)
 fi
