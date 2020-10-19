3bdfe671e6c43e433fcdf6f6852e1a0de9ad7937
Marten Seemann <martenseemann@gmail.com>
2020-10-19 11:40:58 +0700

    ask the user to set the hostname in the install script

--- install.sh
+++ install.sh
@@ -18,6 +18,14 @@ if [[ `uname` == 'Linux' ]]; then
   # install antibody
   curl -sfL git.io/antibody | sh -s - -b /usr/local/bin
 
+  # ask the user to set the hostname
+  orig=$(head -n1 /etc/hostname)
+  read -e -p "Hostname: " -i $orig hostname
+  if [[ "$orig" != "$hostname" ]]; then
+    echo $hostname > /etc/hostname
+    echo "Hostname changed to $hostname. Reboot required to take effect."
+  fi
+
   echo "Setting zsh as default shell"
   chsh -s $(which zsh)
 fi
