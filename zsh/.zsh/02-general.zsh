9353a8042be8b9baa3b73004db0fcf665ecc7d7c
Marten Seemann <martenseemann@gmail.com>
2024-05-23 14:58:59 +0800

    set the language to en_US

--- zsh/.zsh/02-general.zsh
+++ zsh/.zsh/02-general.zsh
@@ -1,4 +1,5 @@
 # Shell options
+export LANG=en_US.utf8
 export TERM=xterm-256color
 export EDITOR=vim
 
@@ -10,3 +11,11 @@ bindkey "^[[1;3D" backward-word
 # correct_all sometimes recommends corrections at the wrong places
 unsetopt correct_all
 setopt correct
+
+# show the host name in the tab title for SSH sessions
+if (( P9K_SSH )); then
+  export PROMPT_COMMAND="echo -ne '\033]0;${USER}@${HOST}\007';$PROMPT_COMMAND"
+else
+  # TODO: reset when disconnecting
+fi
+precmd() { eval "$PROMPT_COMMAND" }
