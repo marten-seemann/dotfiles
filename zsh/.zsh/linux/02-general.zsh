5644c71e095f6e19db24d9efa725c5f994ea2439
Marten Seemann <martenseemann@gmail.com>
2025-12-23 17:08:52 +0800

    remove autojump initialization

--- zsh/.zsh/linux/02-general.zsh
+++
@@ -1,4 +0,0 @@
-# enable autojump
-if [ -f /usr/share/autojump/autojump.sh ]; then
-  . /usr/share/autojump/autojump.sh
-fi
--- zsh/.zsh/mac/02-general.zsh
+++ zsh/.zsh/mac/02-general.zsh
@@ -1,6 +1,3 @@
-# enable autojump
-[[ -s `brew --prefix`/etc/autojump.sh ]] && . `brew --prefix`/etc/autojump.sh
-
 # see https://developer.1password.com/docs/ssh/agent/compatibility/
 # Among others, enables docker builds with --mount=type=ssh
 export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
