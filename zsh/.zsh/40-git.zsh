da71ab0030fdf08920c06eeeb0629ff47ebf8eeb
Marten Seemann <martenseemann@gmail.com>
2026-05-11 18:10:25 +0800

    zsh: make gcm git abbreviation switch to the default branch

--- zsh/.zsh/40-git.zsh
+++ zsh/.zsh/40-git.zsh
@@ -1,5 +1,13 @@
 alias ga='git add'
 alias gcb='git checkout -b'
-alias gcm='git checkout master'
 alias gco='git checkout'
 alias gp='git push'
+
+function gcm {
+  local default
+  default=$(git symbolic-ref --short refs/remotes/origin/HEAD 2>/dev/null) || {
+    echo "gcm: not a git repo or no origin/HEAD set" >&2
+    return 1
+  }
+  git checkout "${default#origin/}"
+}
